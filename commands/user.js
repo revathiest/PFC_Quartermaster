9bbdf1d65738ecb906976d99c2a452a20ad855c6
revat <revat@SLOTH>
2022-09-03 08:44:03 -0500

    Added user command to get org info

--- commands/user.js
+++ commands/user.js
@@ -1,13 +1,13 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
 
 module.exports ={
-	data: {
-		"type":2,
-		"name":"Get RSI Profile"
-	},
+	data: new SlashCommandBuilder()
+		.setName('user')
+		.setDescription('Retrieves user information from the Star Citizen API')
+		.addStringOption(option => option.setName('name').setDescription('The name of the user to look up.')),
 
-	async execute(interaction){
-		
+	async execute(interaction, client){
+
 		// Star Citizen API URL definitions
 		const SCAApiBase = 'https://api.starcitizen-api.com/77210b95720bd50b3584ead32936dfd4/v1/'
 		//API Modes
@@ -22,31 +22,24 @@ module.exports ={
 		const SCApiLiveUser = SCApiLive + SCApiUser
 		const SCApiAutoUser = SCApiAuto + SCApiUser
 		const SCApiEagerUser = SCApiEager + SCApiUser
-		
-		const fetch = require('node-fetch') // required to call the Star Citizen API
-		const { EmbedBuilder } = require ('discord.js') // required to send the embed to Discord
-		
-		var temp
 
-		if (!interaction.targetMember.nickname){
-			temp = interaction.targetUser.username.split(" ")
-		} else {
-			temp = interaction.targetMember.nickname.split(" ")
-		}
-		var username = temp[temp.length - 1]		
-		
+		const fetch = require('node-fetch') // required to call the Star Citizen API
+		const { MessageEmbed } = require ('discord.js') // required to send the embed to Discord
+
+		const username = interaction.options._hoistedOptions[0].value
+
 		var answer = await fetch(SCApiEagerUser + username).then(response => response.text())
 		var user = JSON.parse(answer)
-		
+
 		if (user.data == null ){
 			if (user.data == null){
 				interaction.reply({content: user.message, ephemeral: true})
 				return
 			}
 		}
-		
+
 		if (user.data.profile != undefined) {
-			
+
 			const userName = user.data.profile.display
 			const userURL = user.data.profile.page.url
 			const userImg = user.data.profile.image
@@ -68,30 +61,30 @@ module.exports ={
 			var affOrgRank = 'Rank Unknown'
 			var affOrgImg = null
 			var affOrgSID = null
-				
-			
-			
-			const responseEmbed = new EmbedBuilder()
+
+
+
+			const responseEmbed = new MessageEmbed()
 			.setColor('#0099ff')
 			.setTitle(userOrgRank + ' - ' + userName)
 			.setURL(userURL)
-			.setAuthor({name: userOrg, iconURL:userOrgImg, url:'https://robertsspaceindustries.com/orgs/' + userOrgSID})
+			.setAuthor(userOrg, userOrgImg, 'https://robertsspaceindustries.com/orgs/' + userOrgSID)
 			.setThumbnail(userImg)
 			.setTimestamp()
-			.setFooter({text:'Official PFC Communication', iconURL:'https://i.imgur.com/5sZV5QN.png'})
-			
+			.setFooter('Official PFC Communication', 'https://i.imgur.com/5sZV5QN.png')
+
 			if (user.data.profile.website != undefined) {
 				userBio = userBio + '\n' + user.data.profile.website
 			}
-			
+
 			responseEmbed.setDescription(userBio)
-			
-			
+
+
 			if (user.data.affiliation != null && user.data.affiliation != '') {
-				
+
 				const affil = user.data.affiliation
 				var afflist = null
-				
+
 				affil.forEach(aff => {
 					if (aff.name == null || aff.name == '') {
 						aff.name = 'Redacted'
@@ -103,16 +96,16 @@ module.exports ={
 						afflist =  afflist + '\n' + aff.name + ' - ' + aff.rank
 					}
 				})
-				
-				responseEmbed.addFields({name: 'Organization Affiliations', value:afflist})
-			
+
+				responseEmbed.addField('Organization Affiliations', afflist)
+
 			}
-			
+
 			interaction.user.send({ embeds: [responseEmbed] })
 			interaction.reply({content: 'Check your DMs', ephemeral: true})
-		
+
 		} else {
-			interaction.reply('RSI doesnt recognize that user.')
+			interaction.reply('That user does not exist.')
 		}
 	}
 
---
+++ commands/userOrg.js
@@ -0,0 +1,101 @@
+module.exports ={
+	data: {
+		"type":2,
+		"name":"Get RSI Org"
+	},
+
+	async execute(interaction, client){
+		
+		// Star Citizen API URL definitions
+		const SCAApiBase = 'https://api.starcitizen-api.com/77210b95720bd50b3584ead32936dfd4/v1/'
+		//API Modes
+		const SCApiCache = SCAApiBase + 'cache/'
+		const SCApiLive = SCAApiBase + 'live/'
+		const SCApiAuto = SCAApiBase + 'auto/'
+		const SCApiEager = SCAApiBase + 'eager/'
+		//API Categories
+		const SCApiOrganization = 'organization/'
+		const SCApiUser = 'user/'
+		//Organization
+		const SCApiCacheOrganization = SCApiCache + SCApiOrganization
+		const SCApiLiveOrganization = SCApiLive + SCApiOrganization
+		const SCApiEagerUser = SCApiEager + SCApiUser
+		
+		const fetch = require('node-fetch') // required to call the Star Citizen API
+		const { EmbedBuilder } = require ('discord.js') // required to send the embed to Discord
+
+		var temp
+
+		if (!interaction.targetMember.nickname){
+			temp = interaction.targetUser.username.split(" ")
+		} else {
+			temp = interaction.targetMember.nickname.split(" ")
+		}
+		var username = temp[temp.length - 1]	
+		
+		var answer = await fetch(SCApiEagerUser + username).then(response => response.text())
+		var user = JSON.parse(answer)
+		
+		if (user.data == null ){
+			if (user.data == null){
+				interaction.reply({content: user.message, ephemeral: true})
+				return
+			}
+		}
+
+		var userOrgSID = null
+
+		if (user.data.profile != undefined) {
+			userOrgSID = user.data.organization.sid
+		}
+
+		answer = await fetch(SCApiLiveOrganization + userOrgSID).then(response => response.text())
+		var org = JSON.parse(answer)
+		
+		if (org.data == null ){
+			if (org.data == null){
+				interaction.reply(org.message)
+				return
+			}
+		}
+		
+		if (org.data != undefined) {
+			
+			const orgName = org.data.name
+			const orgURL = org.data.url
+			const orgImg = org.data.banner
+			var orgBio = 'This org has no biographical information'
+			if (org.data.headline != null && org.data.headline != ''){
+				orgBio = org.data.headline.plaintext
+			}
+			const orgLogo = org.data.logo
+			const orgMembers = org.data.members
+			
+			var orgRecruiting 
+			if (org.data.recruiting == true) {
+				orgRecruiting = 'Open'
+			} else {
+				orgRecruiting = 'Closed'
+			}
+			
+			const responseEmbed = new EmbedBuilder()
+			.setColor('#0099ff')
+			.setTitle(orgName + ' - ' + orgMembers + ' members')
+			.setURL(orgURL)
+			//.setAuthor(orgOrg, orgOrgImg, 'https://robertsspaceindustries.com/orgs/' + orgOrgSID)
+			.setThumbnail(orgLogo)
+			.setTimestamp()
+			.setFooter({text:'Official PFC Communication', iconURL:'https://i.imgur.com/5sZV5QN.png'})
+			.setDescription(orgBio)
+			.addFields({name: 'Recruiting Status: ', value:orgRecruiting})
+			
+			
+			interaction.user.send({ embeds: [responseEmbed] })
+			interaction.reply({content: 'Check your DMs', ephemeral: true})
+		
+		} else {
+			interaction.reply(SCApiLiveOrganization + orgname)
+		}
+	}
+
+}

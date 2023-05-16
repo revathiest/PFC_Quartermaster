55cbba01d953877d5bb0446b0f1ef14d8ad9e743
revat <revat@SLOTH>
2023-05-15 23:16:36 -0500

    fixing broken reset

--- commands/reset.js
+++ commands/reset.js
@@ -1,26 +1,35 @@
-async function execute(interaction) {
-  if (!interaction.member.permissions.has('ADMINISTRATOR')) {
-    return interaction.reply({
-      content: 'Only an administrator can do that. Your attempt has been logged.',
-      ephemeral: true
-    });
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { exec } = require('child_process');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('reset')
+    .setDescription('Restarts the Quartermaster (Admin only)'),
+
+  async execute(interaction) {
+    if (!interaction.member.permissions.has('ADMINISTRATOR')) {
+      return interaction.reply({
+        content: 'Only an administrator can do that. Your attempt has been logged.',
+        ephemeral: true
+      });
+    }
+  
+    const username = interaction.member.user.tag;
+    try {
+      console.log(`Server shut down by: ${username}`);
+      await interaction.reply('Resetting...');
+  
+      // Unregister all commands
+      const commands = await interaction.client.application.commands.fetch();
+      await Promise.all(commands.map(command => command.delete()));
+  
+      process.exit(0);
+    } catch (error) {
+      console.error(`Error occurred while trying to shut down the bot: ${error}`);
+      await interaction.reply({
+        content: 'An error occurred while trying to shut down the bot.',
+        ephemeral: true
+      });
+    }
   }
-
-  const username = interaction.member.user.tag;
-  try {
-    console.log(`Server shut down by: ${username}`);
-    await interaction.reply('Resetting...');
-
-    // Unregister all commands
-    const commands = await interaction.client.application.commands.fetch();
-    await Promise.all(commands.map(command => command.delete()));
-
-    process.exit(0);
-  } catch (error) {
-    console.error(`Error occurred while trying to shut down the bot: ${error}`);
-    await interaction.reply({
-      content: 'An error occurred while trying to shut down the bot.',
-      ephemeral: true
-    });
-  }
-}
+};

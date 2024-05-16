ee89d9c58bf6010ae0fc9deaf53f77fc17cfa9dc
revat <revat@SLOTH>
2024-05-16 17:57:17 -0500

    fixed guild settings model

--- models/guild_settings.js
+++ models/guild_settings.js
@@ -1,26 +1,27 @@
 module.exports = (sequelize, DataTypes) => {
-    const GuildSettings = sequelize.define('GuildSettings', {
-      guild_id: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      key: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      value: {
-        type: DataTypes.TEXT,
-        allowNull: false
+  const GuildSettings = sequelize.define('GuildSettings', {
+    guild_id: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    key: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    value: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+  }, {
+    indexes: [
+      {
+        unique: true,
+        fields: ['guild_id', 'key']
       }
-    });
-  
-    GuildSettings.removeAttribute('id'); // Removing the default id attribute
-  
-    GuildSettings.addConstraint('unique_guild_key', {
-      type: 'unique',
-      fields: ['guild_id', 'key']
-    });
-  
-    return GuildSettings;
-  };
-  
+    ]
+  });
+
+  GuildSettings.removeAttribute('id'); // Removing the default id attribute
+
+  return GuildSettings;
+};

36edfcd7ed583e211511721096f27a93520ee5b2
revat <revat@SLOTH>
2024-05-15 21:47:22 -0500

    pathing error

--- botactions/messageCleanup.js
+++ botactions/messageCleanup.js
@@ -1,26 +1,23 @@
 const fs = require('fs');
+const path = require('path');
 
-/**
- * Deletes messages older than a specified number of days in certain channels.
- * @param {Client} client The Discord client instance.
- */
 async function deleteMessages(client) {
     try {
-        const channelsData = fs.readFileSync('../snapchannels.json');
+        // Properly construct the path to the file
+        const filePath = path.join(__dirname, '..', 'snapchannels.json');
+        const channelsData = fs.readFileSync(filePath);
         const channels = JSON.parse(channelsData);
 
         for (const channelInfo of channels) {
             const channel = await client.channels.fetch(channelInfo.channelId);
 
-            if (channel && (channel.type === 0 || channel.type === 5)) {  // 0 for GUILD_TEXT and 5 for GUILD_NEWS
+            if (channel && (channel.type === 0 || channel.type === 5)) {
                 const messages = await channel.messages.fetch({ limit: 100 });
                 const purgeTime = new Date();
                 purgeTime.setDate(purgeTime.getDate() - channelInfo.purgeTimeInDays);
 
-                // Filter messages based on their timestamps
                 const messagesToDelete = messages.filter(msg => msg.createdTimestamp <= purgeTime.getTime());
 
-                // Bulk delete messages and log the action
                 if (messagesToDelete.size > 0) {
                     await channel.bulkDelete(messagesToDelete, true);
                     console.log(`Deleted ${messagesToDelete.size} messages in channel ${channel.name}`);

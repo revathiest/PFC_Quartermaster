c89c21a59ddea0989d2c16ad6c736860c9353f7e
revat <revat@SLOTH>
2024-05-17 18:18:32 -0500

    fixing scheduler bugs

--- botactions/scheduleHandler.js
+++ botactions/scheduleHandler.js
@@ -2,17 +2,22 @@ const ScheduledAnnouncement = require('../models/scheduledAnnouncementModel');
 
 const saveAnnouncementToDatabase = async (channelId, guildId, embedData, time) => {
   try {
-    await ScheduledAnnouncement.create({ channelId, guildId, embedData: JSON.stringify(embedData), time });
+    await ScheduledAnnouncement.create({
+      channelId,
+      guildId: String(guildId),  // Ensure guildId is a string
+      embedData: JSON.stringify(embedData),
+      time: String(time)  // Ensure time is a string and not null
+    });
     console.log(`Announcement saved to database: channelId=${channelId}, guildId=${guildId}, embedData=${JSON.stringify(embedData)}, time=${time}`);
   } catch (error) {
     console.error('Error saving announcement to database:', error);
   }
 };
 
-const getScheduledAnnouncements = async (guildId) => {
+const getScheduledAnnouncements = async () => {
   try {
-    const announcements = await ScheduledAnnouncement.findAll({ where: { guildId } });
-    console.log(`Retrieved ${announcements.length} scheduled announcements from database for guildId=${guildId}`);
+    const announcements = await ScheduledAnnouncement.findAll();
+    console.log(`Retrieved ${announcements.length} scheduled announcements from database`);
     return announcements;
   } catch (error) {
     console.error('Error retrieving scheduled announcements from database:', error);

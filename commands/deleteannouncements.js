9e3c3e65e5ab6f5843940c36503dca17208f4a72
revat <revat@SLOTH>
2024-05-17 23:29:19 -0500

    restrict list and delete announcements to admin

--- commands/deleteannouncements.js
+++ commands/deleteannouncements.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { deleteScheduledAnnouncement } = require('../botactions/scheduleHandler');
 
+const allowedRoles = ['Admiral', 'Fleet Admiral'];
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('deleteannouncement')
@@ -10,6 +12,12 @@ module.exports = {
                 .setDescription('The ID of the announcement to delete')
                 .setRequired(true)),
     async execute(interaction) {
+        const memberRoles = interaction.member.roles.cache.map(role => role.name);
+        if (!allowedRoles.some(role => memberRoles.includes(role))) {
+            await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+            return;
+        }
+
         const id = interaction.options.getString('id');
         const success = await deleteScheduledAnnouncement(id);
 
--- commands/listannouncements.js
+++ commands/listannouncements.js
@@ -1,11 +1,19 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { getScheduledAnnouncements } = require('../botactions/scheduleHandler');
 
+const allowedRoles = ['Admiral', 'Fleet Admiral'];
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('listannouncements')
         .setDescription('List all scheduled announcements'),
     async execute(interaction) {
+        const memberRoles = interaction.member.roles.cache.map(role => role.name);
+        if (!allowedRoles.some(role => memberRoles.includes(role))) {
+            await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+            return;
+        }
+
         const announcements = await getScheduledAnnouncements();
         if (!announcements.length) {
             await interaction.reply('There are no scheduled announcements.');

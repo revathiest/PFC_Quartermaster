38a9c1d55b1831fe61dc30275d2ca33389f3aaea
revat <revat@SLOTH>
2024-05-18 10:43:49 -0500

    fixing a bug created when updating /org

--- botactions/commandRegistration.js
+++ botactions/commandRegistration.js
@@ -9,17 +9,22 @@ const rest = new REST({ version: '9' }).setToken(token);
 async function registerCommands(client) {
     client.commands = new Map();
     const cmdsToRegister = [];
-    
+
     const commandsPath = path.join(__dirname, '..', 'commands');
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));    
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
     console.log('Registering commands:');
     for (const file of commandFiles) {
         const command = require(`../commands/${file}`);
         try {
-            client.commands.set(command.data.name, command);
-            cmdsToRegister.push(command.data.toJSON());
-            console.log(`Registered command ${command.data.name}`);
+            // Check if command.data has a toJSON method
+            if (typeof command.data.toJSON === 'function') {
+                client.commands.set(command.data.name, command);
+                cmdsToRegister.push(command.data.toJSON());
+                console.log(`Registered command ${command.data.name}`);
+            } else {
+                throw new TypeError(`command.data.toJSON is not a function for ${command.data.name}`);
+            }
         } catch (error) {
             console.error(`Error registering command ${command.data.name}:`, error);
         }

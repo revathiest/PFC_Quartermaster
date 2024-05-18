bf1354f581c433cc27877d48280be40922ddcf5b
revathiest <revathiest@gmail.com>
2024-05-18 14:01:47 -0500

    order of operations error

--- config/database.js
+++ config/database.js
@@ -4,14 +4,15 @@ const config = require('../databaseConfig.json');  // Adjust the path if needed
 const env = process.env.NODE_ENV || 'development';
 const dbConfig = config[env];
 
-const UsageLog = require('../models/usageLog')(sequelize);
-const VoiceLog = require('../models/voiceLog')(sequelize);
-
 const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
     host: dbConfig.host,
     dialect: dbConfig.dialect,
     logging: dbConfig.logging || false,
 });
+
+// Import models here, after initializing sequelize
+const UsageLog = require('../models/usageLog')(sequelize);
+const VoiceLog = require('../models/voiceLog')(sequelize);
 
 const initializeDatabase = async () => {
     try {
@@ -21,7 +22,6 @@ const initializeDatabase = async () => {
         
         await sequelize.sync({ force: false });  // Set to true only if you want to drop and recreate tables
         console.log('Database synchronized');
-
     } catch (error) {
         console.error('Unable to synchronize the database:', error);
     }

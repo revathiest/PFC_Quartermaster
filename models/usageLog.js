c99761a05a0eb33d6794ea91b93d4c09c1986daa
revathiest <revathiest@gmail.com>
2024-05-19 15:56:08 -0500

    adding message ID when saving message events

--- models/usageLog.js
+++ models/usageLog.js
@@ -14,6 +14,10 @@ module.exports = (sequelize) => {
             type: DataTypes.STRING,
             allowNull: false,
         },
+        message_jid: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
         message_content: {
             type: DataTypes.TEXT,
             allowNull: true,

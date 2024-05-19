d6ec4beba6ce6da62ad1b0b033244fe26c25b85f
revathiest <revathiest@gmail.com>
2024-05-19 17:51:40 -0500

    updatiing snap channels

--- commands/addsnapchannel.js
+++ commands/addsnapchannel.js
@@ -8,12 +8,27 @@ module.exports = {
         .addStringOption(option =>
             option.setName('channel')
                 .setDescription('The name of the snap channel')
-                .setRequired(true)),
+                .setRequired(true))
+        .addIntegerOption(option =>
+            option.setName('purgetime')
+                .setDescription('Purge time in days (default: 30)')
+                .setRequired(false)),
     async execute(interaction) {
         try {
             const channelName = interaction.options.getString('channel');
-            addSnapChannel(channelName);
-            await interaction.reply(`Snap channel ${channelName} added.`);
+            const purgeTimeInDays = interaction.options.getInteger('purgetime') || 30; // Default to 30 days
+            const guild = interaction.guild;
+            const channel = guild.channels.cache.find(ch => ch.name === channelName);
+
+            if (!channel) {
+                return await interaction.reply({ content: `Channel ${channelName} not found.`, ephemeral: true });
+            }
+
+            const channelId = channel.id;
+            const serverId = guild.id;
+
+            await addSnapChannel(channelId, purgeTimeInDays, serverId);
+            await interaction.reply(`Snap channel ${channelName} (ID: ${channelId}) added with a purge time of ${purgeTimeInDays} days.`);
         } catch (error) {
             console.error(error);
             await interaction.reply({ content: 'There was an error while adding the snap channel.', ephemeral: true });
--- commands/removesnapchannel.js
+++ commands/removesnapchannel.js
@@ -12,8 +12,17 @@ module.exports = {
     async execute(interaction) {
         try {
             const channelName = interaction.options.getString('channel');
-            removeSnapChannel(channelName);
-            await interaction.reply(`Snap channel ${channelName} removed.`);
+            const guild = interaction.guild;
+            const channel = guild.channels.cache.find(ch => ch.name === channelName);
+
+            if (!channel) {
+                return await interaction.reply({ content: `Channel ${channelName} not found.`, ephemeral: true });
+            }
+
+            const channelId = channel.id;
+
+            await removeSnapChannel(channelId);
+            await interaction.reply(`Snap channel ${channelName} (ID: ${channelId}) removed.`);
         } catch (error) {
             console.error(error);
             await interaction.reply({ content: 'There was an error while removing the snap channel.', ephemeral: true });

c0478f1928ba1b15a4559555d241f5185f5827fd
revathiest <revathiest@gmail.com>
2024-05-18 22:12:52 -0500

    added reaction remove

--- botactions/eventHandling/reactionEvents.js
+++ botactions/eventHandling/reactionEvents.js
@@ -21,5 +21,26 @@ module.exports = {
         } catch (error) {
             console.error('Error logging reaction add:', error);
         }
+    },
+    handleReactionRemove: async function(reaction, user) {
+        if (user.bot) return; // Ignore bot reactions
+
+        const { message, emoji } = reaction;
+        const serverId = message.guild.id;
+
+        try {
+            await UsageLog.create({
+                user_id: user.id,
+                interaction_type: 'reaction',
+                event_type: 'reaction_remove',
+                reaction_type: emoji.name,
+                channel_id: message.channel.id,
+                server_id: serverId,
+                event_time: new Date(),
+            });
+            console.log('Reaction remove logged successfully');
+        } catch (error) {
+            console.error('Error logging reaction add:', error);
+        }
     }
 };

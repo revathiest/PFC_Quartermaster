add12c781cec03cf893a3481a54eee245e630e24
revathiest <revathiest@gmail.com>
2025-04-06 12:45:08 -0500

    more debugging;

--- botactions/userManagement/permissions.js
+++ botactions/userManagement/permissions.js
@@ -6,7 +6,21 @@
  * @returns {boolean} - True if the user is an admin, false otherwise.
  */
  function isAdmin(interaction) {
+
+    if (!interaction){
+        console.log('[PERMISSIONS] No interaction defined.');
+        return false;
+    }
+    if (!interaction.member){
+        console.log('[PERMISSIONS] No member defined in interaction.');
+        return false;
+    }
+
     const member = interaction.member;
+
+    if (!member){
+        console.log('[PERMISSIONS] Unable to extract member.');
+    }
     
     if (member.permissions && member.permissions.has('Administrator')) {
         return true;

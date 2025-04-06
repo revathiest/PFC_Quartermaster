934a22f324040804f9ca5daf2ffc858180737b9e
revathiest <revathiest@gmail.com>
2025-04-06 15:52:30 -0500

    Updating the Manufacturer table to have an id key

--- models/manufacturer.js
+++ models/manufacturer.js
@@ -2,18 +2,22 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
   return sequelize.define('Manufacturer', {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
     code: {
       type: DataTypes.STRING,
-      primaryKey: true,
-      allowNull: false
+      allowNull: true
     },
     name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: true
     },
     link: {
       type: DataTypes.STRING(1024),
-      allowNull: false
+      allowNull: true
     }
   }, {
     tableName: 'Manufacturers',

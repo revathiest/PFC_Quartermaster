e029c733b9013d8a5a03af0ece92e39bf5828c12
revathiest <revathiest@gmail.com>
2025-04-06 11:51:09 -0500

    updated scheduleHandler

--- botactions/scheduling/scheduleHandler.js
+++ botactions/scheduling/scheduleHandler.js
@@ -1,5 +1,4 @@
-const { sequelize } = require('../../config/database');
-const ScheduledAnnouncement = require('../../models/scheduledAnnouncement');
+const { sequelize, ScheduledAnnouncement } = require('../../config/database');
 const { getChannelNameById, getGuildNameById } = require('../utilityFunctions');
 
 const saveAnnouncementToDatabase = async (channelId, guildId, embedData, time, client) => {

47a99c242f1aafc7f0718e34f0a407697c4668da
revathiest <revathiest@gmail.com>
2025-04-06 13:57:14 -0500

    added debugging

--- utils/apiSync/manufacturers.js
+++ utils/apiSync/manufacturers.js
@@ -8,6 +8,8 @@ async function syncManufacturers() {
     const initialResponse = await fetchSCData('manufacturers');
     const total = initialResponse?.meta?.total;
 
+    console.log('[DEBUG] API Response: ', initialResponse);
+
     if (!total || typeof total !== 'number') {
       throw new Error('Could not determine total manufacturers count');
     }

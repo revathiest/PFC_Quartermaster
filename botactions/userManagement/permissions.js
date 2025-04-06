85c9826aff341cf60f9a7613a08e698ad1689b46
revathiest <revathiest@gmail.com>
2025-04-06 12:42:23 -0500

    corrected isAdmin function

--- botactions/userManagement/permissions.js
+++ botactions/userManagement/permissions.js
@@ -6,9 +6,11 @@
  * @returns {boolean} - True if the user is an admin, false otherwise.
  */
  function isAdmin(interaction) {
+    const member = interaction.member;
     
-    console.log('[PERMISSIONS] Member perms:', interaction.member?.permissions?.toArray());
-    return interaction.member?.permissions?.has?.('Administrator');
+    if (member.permissions && member.permissions.has('Administrator')) {
+        return true;
+      }
   }
   
   module.exports = {

18fa3d83a71f6e4603bbd58d99e8221b8519e5f3
revathiest <revathiest@gmail.com>
2025-04-07 11:22:37 -0500

    remove unnecessary configuration declaration

--- botactions/eventHandling/messageEvents.js
+++ botactions/eventHandling/messageEvents.js
@@ -2,10 +2,6 @@ const { UsageLog } = require('../../config/database');
 const filter = require('../../messages.json');
 const OpenAI = require('openai');
 
-
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 module.exports = {

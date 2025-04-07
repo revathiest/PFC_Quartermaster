ae08ecdd4b902b397d9893b7b464abb75948f16a
revathiest <revathiest@gmail.com>
2025-04-07 04:39:33 -0500

    updated upsert to save all data

--- commands/shipdetails.js
+++ commands/shipdetails.js
@@ -95,38 +95,95 @@ module.exports = {
         }
 
         await VehicleDetail.upsert({
-          uuid: parsed.uuid || vehicle.uuid,
-          name: parsed.name,
-          slug: parsed.slug,
-          class_name: parsed.class_name,
-          cargo_capacity: parsed.cargo_capacity,
-          shield_hp: parsed.shield_hp,
-          crew_min: parsed.crew?.min ?? null,
-          crew_max: parsed.crew?.max ?? null,
-          length: parsed.sizes?.length ?? null,
-          beam: parsed.sizes?.beam ?? null,
-          height: parsed.sizes?.height ?? null,
-          ir_emission: parsed.emission?.ir ?? null,
-          em_idle: parsed.emission?.em_idle ?? null,
-          em_max: parsed.emission?.em_max ?? null,
-          vehicle_inventory: parsed.vehicle_inventory ?? null,
-          personal_inventory: parsed.personal_inventory ?? null,
-          speed_scm: parsed.speed?.scm ?? null,
-          speed_max: parsed.speed?.max ?? null,
-          agility_pitch: parsed.agility?.pitch ?? null,
-          agility_yaw: parsed.agility?.yaw ?? null,
-          agility_roll: parsed.agility?.roll ?? null,
-          quantum_speed: parsed.quantum?.quantum_speed ?? null,
-          quantum_range: parsed.quantum?.quantum_range ?? null,
-          focus: parsed.foci?.[0]?.en_EN ?? null,
-          type: parsed.type?.en_EN ?? null,
-          description: parsed.description?.en_EN ?? null,
-          manufacturer: parsed.manufacturer?.name ?? null,
-          production_status: parsed.production_status?.en_EN ?? null,
-          size: parsed.size?.en_EN ?? null,
-          version: parsed.version,
-          updated_at: parsed.updated_at
-        });
+            uuid: parsed.uuid || vehicle.uuid,
+            name: parsed.name,
+            slug: parsed.slug,
+            class_name: parsed.class_name,
+            length: parsed.sizes?.length ?? null,
+            beam: parsed.sizes?.beam ?? null,
+            height: parsed.sizes?.height ?? null,
+          
+            ir_emission: parsed.emission?.ir ?? null,
+            em_idle: parsed.emission?.em_idle ?? null,
+            em_max: parsed.emission?.em_max ?? null,
+          
+            mass: parsed.mass ?? null,
+            cargo_capacity: parsed.cargo_capacity ?? null,
+            vehicle_inventory: parsed.vehicle_inventory ?? null,
+            personal_inventory: parsed.personal_inventory ?? null,
+          
+            crew_min: parsed.crew?.min ?? null,
+            crew_max: parsed.crew?.max ?? null,
+            crew_weapon: parsed.crew?.weapon ?? null,
+            crew_operation: parsed.crew?.operation ?? null,
+          
+            health: parsed.health ?? null,
+            shield_hp: parsed.shield_hp ?? null,
+          
+            speed_scm: parsed.speed?.scm ?? null,
+            speed_max: parsed.speed?.max ?? null,
+            speed_zero_to_scm: parsed.speed?.zero_to_scm ?? null,
+            speed_zero_to_max: parsed.speed?.zero_to_max ?? null,
+            speed_scm_to_zero: parsed.speed?.scm_to_zero ?? null,
+            speed_max_to_zero: parsed.speed?.max_to_zero ?? null,
+          
+            fuel_capacity: parsed.fuel?.capacity ?? null,
+            fuel_intake_rate: parsed.fuel?.intake_rate ?? null,
+            fuel_usage_main: parsed.fuel?.usage?.main ?? null,
+            fuel_usage_maneuvering: parsed.fuel?.usage?.maneuvering ?? null,
+            fuel_usage_retro: parsed.fuel?.usage?.retro ?? null,
+            fuel_usage_vtol: parsed.fuel?.usage?.vtol ?? null,
+          
+            quantum_speed: parsed.quantum?.quantum_speed ?? null,
+            quantum_spool_time: parsed.quantum?.quantum_spool_time ?? null,
+            quantum_fuel_capacity: parsed.quantum?.quantum_fuel_capacity ?? null,
+            quantum_range: parsed.quantum?.quantum_range ?? null,
+          
+            agility_pitch: parsed.agility?.pitch ?? null,
+            agility_yaw: parsed.agility?.yaw ?? null,
+            agility_roll: parsed.agility?.roll ?? null,
+            agility_acceleration_main: parsed.agility?.acceleration?.main ?? null,
+            agility_acceleration_retro: parsed.agility?.acceleration?.retro ?? null,
+            agility_acceleration_vtol: parsed.agility?.acceleration?.vtol ?? null,
+            agility_acceleration_maneuvering: parsed.agility?.acceleration?.maneuvering ?? null,
+            agility_main_g: parsed.agility?.acceleration?.main_g ?? null,
+            agility_retro_g: parsed.agility?.acceleration?.retro_g ?? null,
+            agility_vtol_g: parsed.agility?.acceleration?.vtol_g ?? null,
+            agility_maneuvering_g: parsed.agility?.acceleration?.maneuvering_g ?? null,
+          
+            armor_ir: parsed.armor?.signal_infrared ?? null,
+            armor_em: parsed.armor?.signal_electromagnetic ?? null,
+            armor_cross_section: parsed.armor?.signal_cross_section ?? null,
+            armor_physical: parsed.armor?.damage_physical ?? null,
+            armor_energy: parsed.armor?.damage_energy ?? null,
+            armor_distortion: parsed.armor?.damage_distortion ?? null,
+            armor_thermal: parsed.armor?.damage_thermal ?? null,
+            armor_biochemical: parsed.armor?.damage_biochemical ?? null,
+            armor_stun: parsed.armor?.damage_stun ?? null,
+          
+            focus: parsed.foci?.[0]?.en_EN ?? null,
+            type: parsed.type?.en_EN ?? null,
+            description: parsed.description?.en_EN ?? null,
+          
+            size_class: parsed.size_class ?? null,
+            manufacturer_name: parsed.manufacturer?.name ?? null,
+            manufacturer_code: parsed.manufacturer?.code ?? null,
+          
+            insurance_claim_time: parsed.insurance?.claim_time ?? null,
+            insurance_expedite_time: parsed.insurance?.expedite_time ?? null,
+            insurance_expedite_cost: parsed.insurance?.expedite_cost ?? null,
+          
+            production_status: parsed.production_status?.en_EN ?? null,
+            production_note: parsed.production_note?.en_EN ?? null,
+            size: parsed.size?.en_EN ?? null,
+          
+            msrp: parsed.msrp ?? null,
+            pledge_url: parsed.pledge_url ?? null,
+          
+            updated_at: parsed.updated_at,
+            version: parsed.version
+          });
+          
 
         detail = await VehicleDetail.findByPk(vehicle.uuid);
       } catch (err) {

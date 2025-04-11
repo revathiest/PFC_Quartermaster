3682579c6d4377793eea8877a07f5a43efee52a9
revathiest <revathiest@gmail.com>
2025-04-11 06:37:10 -0500

    fixed table names

--- commands/uexfinditem.js
+++ commands/uexfinditem.js
@@ -1,9 +1,9 @@
 // File: commands/uexfinditem.js
 const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder, EmbedBuilder } = require('discord.js');
 const { Op } = require('sequelize');
-const ItemsPricesAll = require('../models/itemsPricesAll');
-const CommoditiesPricesAll = require('../models/commoditiesPricesAll');
-const VehiclesPurchasesPricesAll = require('../models/vehiclesPurchasesPricesAll');
+const ItemsPricesAll = require('../models/itemsPrices');
+const CommoditiesPricesAll = require('../models/commoditiesPrices');
+const VehiclesPurchasesPricesAll = require('../models/vehiclesPurchasesPrices');
 const UexTerminal = require('../models/uexTerminal');
 const { buildUexAvailabilityEmbed } = require('../components/embedBuilders/uexAvailabilityEmbed');
 

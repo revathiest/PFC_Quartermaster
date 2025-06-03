5de869ccd98be209c271c328a9c923a6f2d87e9b
revathiest <revathiest@gmail.com>
2025-06-03 14:34:49 -0500

    fix: restrict hunt set-channels to admins (#269)

--- commands/hunt/set-channels.js
+++ commands/hunt/set-channels.js
@@ -1,10 +1,18 @@
-const { SlashCommandSubcommandBuilder, ChannelType, MessageFlags } = require('discord.js');
+const {
+  SlashCommandSubcommandBuilder,
+  ChannelType,
+  PermissionFlagsBits,
+  MessageFlags
+} = require('discord.js');
 const { Config } = require('../../config/database');
+
+const allowedRoles = ['Admiral', 'Fleet Admiral'];
 
 module.exports = {
   data: () => new SlashCommandSubcommandBuilder()
     .setName('set-channels')
     .setDescription('Configure hunt activity and review channels')
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
     .addChannelOption(opt =>
       opt.setName('activity')
         .setDescription('Channel where /hunt commands are allowed')
@@ -17,6 +25,12 @@ module.exports = {
         .setRequired(true)),
 
   async execute(interaction) {
+    const memberRoles = interaction.member?.roles?.cache?.map(r => r.name) || [];
+    if (!allowedRoles.some(r => memberRoles.includes(r))) {
+      await interaction.reply({ content: 'You do not have permission to use this command.', flags: MessageFlags.Ephemeral });
+      return;
+    }
+
     const activity = interaction.options.getChannel('activity');
     const review = interaction.options.getChannel('review');
     const botType = process.env.BOT_TYPE || 'development';

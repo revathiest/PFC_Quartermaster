9c92f3d1dcca31ee59f600004c31bab7873572db
revathiest <revathiest@gmail.com>
2025-06-03 13:28:38 -0500

    feat(hunt): expand player help

--- __tests__/commands/hunt/help.test.js
+++ __tests__/commands/hunt/help.test.js
@@ -1,11 +1,18 @@
 const help = require('../../../commands/hunt/help');
 const { MessageFlags } = require('../../../__mocks__/discord.js');
 
-test('replies with help embed', async () => {
+test('replies with player help embed', async () => {
   const interaction = { reply: jest.fn() };
   await help.execute(interaction);
-  expect(interaction.reply).toHaveBeenCalledWith({
-    embeds: [expect.any(Object)],
-    flags: MessageFlags.Ephemeral
-  });
+
+  const reply = interaction.reply.mock.calls[0][0];
+  expect(reply.flags).toBe(MessageFlags.Ephemeral);
+  const fields = reply.embeds[0].data.fields.map(f => f.name);
+  expect(fields).toEqual([
+    '/hunt list',
+    '/hunt poi list',
+    '/hunt score [user]',
+    '/hunt leaderboard',
+    '/hunt help'
+  ]);
 });
